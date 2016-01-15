38e8571599d78c582eb7a2b330664bebbd09f3b7
Christian Petrov <cpetrov@eclipsesource.com>
2016-01-15 17:01:03 +0100

    Refactor Drawer
    Improve naming and general comprehensibility.

--- src/ui/Drawer.js
+++ src/ui/Drawer.js
@@ -10,62 +10,63 @@ exports.create = function() {
 
   DrawerUserArea.create().on("tap", toggleAccountMode).appendTo(scrollView);
 
-  var drawerListItems = createDrawerListItems();
-  var accountItems = createAccountItems();
+  var drawerList = createDrawerList();
+  var accountList = createAccountList();
 
   tabris.ui.on("change:activePage", function() {
-    drawerListItems.children()
+    drawerList.children()
       .filter(function(child) {
-        return child.get("item") instanceof tabris.Page;
+        return child.get("page") instanceof tabris.Page;
       })
-      .forEach(function(drawerPageItem) {
-        drawerPageItem.find("#iconImageView").set("image", getPageItemImageVariant(drawerPageItem));
-        drawerPageItem.set("background", getPageItemBackground(drawerPageItem));
+      .forEach(function(pageItem) {
+        pageItem.updateSelection();
       });
   });
 
   function toggleAccountMode(userArea) {
-    accountItems.set("visible", !accountModeEnabled);
-    drawerListItems.set("visible", accountModeEnabled);
+    accountList.set("visible", !accountModeEnabled);
+    drawerList.set("visible", accountModeEnabled);
     userArea.find("#menuArrowImageView").set("transform", !accountModeEnabled ? {rotation: Math.PI} : null);
     accountModeEnabled = !accountModeEnabled;
   }
 
-  function createDrawerListItems() {
-    var drawerListItems = tabris.create("Composite", {
+  function createDrawerList() {
+    var drawerList = tabris.create("Composite", {
       left: 0, top: "#userArea 8", right: 0
     }).appendTo(scrollView);
-    createPageItem("My Schedule", "schedulePage").appendTo(drawerListItems);
-    createPageItem("Explore", "explorePage").appendTo(drawerListItems);
-    createPageItem("Map", "mapPage").appendTo(drawerListItems);
-    createSeparator().appendTo(drawerListItems);
-    createPageItem("Settings", "settingsPage").appendTo(drawerListItems);
-    return drawerListItems;
+    createPageListItem("My Schedule", "schedulePage").appendTo(drawerList);
+    createPageListItem("Explore", "explorePage").appendTo(drawerList);
+    createPageListItem("Map", "mapPage").appendTo(drawerList);
+    createSeparator().appendTo(drawerList);
+    createPageListItem("Settings", "settingsPage").appendTo(drawerList);
+    return drawerList;
   }
 
-  function createPageItem(name, id) {
+  function createPageListItem(name, id) {
     var page = tabris.ui.find("#" + id).first();
-    var itemComposite = createItem(page.get("title"), page.get("image"))
-      .set({
-        item: page,
-        background: pageIsActive(page) ? "#e3e3e3" : "transparent"
-      })
+    var pageListItem = createListItem(page.get("title"), page.get("image"));
+    pageListItem.updateSelection = function() {
+      pageListItem.find("#iconImageView").set("image", getPageImageVariant(page));
+      pageListItem.set("background", pageIsActive(page) ? colors.LIGHT_BACKGROUND_COLOR : "transparent");
+    };
+    pageListItem.set("page", page)
       .on("tap", function() {
         if (tabris.ui.drawer) {
           tabris.ui.drawer.close();
         }
         page.open();
       });
-    return itemComposite;
+    pageListItem.updateSelection();
+    return pageListItem;
   }
 
-  function createAccountItems() {
-    var accountItems = tabris.create("Composite", {
+  function createAccountList() {
+    var accountList = tabris.create("Composite", {
       left: 0, top: "#userArea 8", right: 0,
       visible: false
     }).appendTo(scrollView);
-    createItem("Logout", {src: "resources/images/logout.png", scale: 2}).appendTo(accountItems);
-    return accountItems;
+    createListItem("Logout", {src: "resources/images/logout.png", scale: 2}).appendTo(accountList);
+    return accountList;
   }
 
   function createSeparator() {
@@ -78,7 +79,7 @@ exports.create = function() {
     return container;
   }
 
-  function createItem(text, image) {
+  function createListItem(text, image) {
     var container = tabris.create("Composite", {
       left: 0, top: "prev()", right: 0, height: 48,
       highlightOnTouch: true
@@ -98,8 +99,7 @@ exports.create = function() {
     return container;
   }
 
-  function getPageItemImageVariant(pageItem) {
-    var page = pageItem.get("item");
+  function getPageImageVariant(page) {
     var pageImage = page.get("image");
     if(pageIsActive(page)) {
       var imageSource = pageImage.src.replace(/.png$/, "_dark.png");
@@ -107,11 +107,6 @@ exports.create = function() {
     }
     return pageImage;
   }
-
-  function getPageItemBackground(pageItem) {
-    return pageIsActive(pageItem.get("item")) ? "#e3e3e3" : "transparent";
-  }
-
 
   function pageIsActive(page) {
     return tabris.ui.get("activePage").get("id") === page.get("id");
