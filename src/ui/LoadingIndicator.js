1492d516c234df721a06350d03e03398c8554f0c
Christian Petrov <cpetrov@eclipsesource.com>
2016-02-03 10:15:51 +0100

    Show activity indicator on all platforms
    It will be displayed during fetching of data.

--- src/ui/LoadingIndicator.js
+++ src/ui/LoadingIndicator.js
@@ -1,20 +1,8 @@
-var create = {
-  Android: function() {
-    return tabris.create("Composite"); // stub
-  },
-  iOS: function(options) {
-    var composite = tabris.create("Composite", {
-      left: 0, top: 0, right: 0, bottom: 0,
-      background: options && options.shade ? "white" : "transparent"
-    });
-    tabris.create("ActivityIndicator", {centerX: 0, centerY: 0}).appendTo(composite);
-    return composite;
-  }
-};
-
 exports.create = function(options) {
-  if (["Android", "iOS"].indexOf(device.platform) > -1) {
-    return create[device.platform](options);
-  }
-  return tabris.create("Composite"); // not implemented for this platform
+  var composite = tabris.create("Composite", {
+    left: 0, top: 0, right: 0, bottom: 0,
+    background: options && options.shade ? "white" : "transparent"
+  });
+  tabris.create("ActivityIndicator", {centerX: 0, centerY: 0}).appendTo(composite);
+  return composite;
 };
