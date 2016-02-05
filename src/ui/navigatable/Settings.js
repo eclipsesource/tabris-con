3f9c4228d1f74c965cd2189db8eace0eca06c5c4
Christian Petrov <cpetrov@eclipsesource.com>
2016-02-05 20:26:49 +0100

    Show "TBD" in unfinished sections
    Otherwise they look broken.

--- src/ui/navigatable/Settings.js
+++ src/ui/navigatable/Settings.js
@@ -1,11 +1,19 @@
 var getImage = require("../../getImage");
 var Navigatable = require("./Navigatable");
+var fontToString = require("../../fontToString");
+var sizes = require("../../../resources/sizes");
 
 exports.create = function() {
-  return Navigatable.create({
+  var settings = Navigatable.create({
     id: "settings",
     title: "Settings",
     image: getImage("settings"),
     left: 0, top: 0, right: 0, bottom: 0
   });
+  tabris.create("TextView", {
+    font: fontToString({size: sizes.FONT_XXLARGE, weight: "bold"}),
+    text: "TBD",
+    centerX: 0, centerY: 0
+  }).appendTo(settings);
+  return settings;
 };
