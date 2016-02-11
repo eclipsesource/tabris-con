0ae97de7d9506fa4cfff3dc97fe856f9514e6dba
Christian Petrov <cpetrov@eclipsesource.com>
2016-02-11 17:41:31 +0100

    Rework map image loading on iOS
    Currently we cannot reference bundled resources in the WebView on iOS.
    As a workaround:
    
    * Load images from remote resource on iOS.
    * When no internet connection is available, create an ImageView with a
    non-zoomable map instead.
    
    This is part of a workaround implementation addressing a missing
    ImageView feature: eclipsesource/tabris-js#775.

--- src/ui/navigatable/Map.js
+++ src/ui/navigatable/Map.js
@@ -1,5 +1,8 @@
+/* globals fetch: false, Promise: true*/
 var getImage = require("../../getImage");
 var Navigatable = require("./Navigatable");
+Promise = require("promise");
+require("whatwg-fetch");
 
 exports.create = function() {
   var map = Navigatable.create({
@@ -9,23 +12,69 @@ exports.create = function() {
     left: 0, top: 0, right: 0, bottom: 0,
     background: "#cdcbcc"
   });
-  var imageSrc = tabris.app.getResourceLocation("resources/images/floorplan.png");
-  var jQuerySrc = tabris.app.getResourceLocation("lib/jquery-2.2.0.min.js");
-  var jQueryPanZoomSrc = tabris.app.getResourceLocation("lib/jquery.panzoom.min.js");
-  tabris.create("WebView", {
-    left: 0, top: 0, right: 0, bottom: 0,
-    background: "#cdcbcc",
-    html: "<html>" +
-            "<head>" +
-              "<style>body {background-color: #cdcbcc;}</style>" +
-              "<script type='text/javascript' src='" + jQuerySrc + "'></script>" +
-              "<script type='text/javascript' src='" + jQueryPanZoomSrc + "'></script>" +
-            "</head>" +
-            "<body>" +
-              "<img src='" + imageSrc + "' />" +
-              "<script>$(document).ready(function(){$('img').panzoom('fit': $('#fit'));});</script>" +
-            "</body>" +
-          "</html>"
-  }).appendTo(map);
+  if (device.platform === "Android") {
+    createWebViewMapContainer(map).set("html", getWebviewHtml(getBundledMapResource()));
+  } else {
+    internetConnectionAvailable()
+      .then(function() {createWebViewMapContainer(map).set("html", getWebviewHtml(getRemoteMapResource()));})
+      .catch(function() {createImageViewMapContainer(map);});
+  }
   return map;
 };
+
+function createWebViewMapContainer(map) {
+  return tabris.create("WebView", {
+    left: 0, top: 0, right: 0, bottom: 0,
+    background: "#cdcbcc"
+  }).appendTo(map);
+}
+
+function createImageViewMapContainer(map) {
+  tabris.create("ImageView", {
+    left: 0, top: 0, right: 0, bottom: 0,
+    image: "../../../resources/images/floorplan.png"
+  }).appendTo(map);
+}
+
+function getWebviewHtml(mapResource) {
+  return "<html>" +
+    "<head>" +
+      "<style>body {background-color: #cdcbcc;}</style>" +
+      "<script type='text/javascript' src='" + mapResource.jQuery + "'></script>" +
+      "<script type='text/javascript' src='" + mapResource.jQueryPanZoom + "'></script>" +
+    "</head>" +
+    "<body>" +
+      "<img src='" + mapResource.image + "' />" +
+      "<script>$(document).ready(function(){$('img').panzoom('fit': $('#fit'));});</script>" +
+    "</body>" +
+  "</html>";
+}
+
+function getBundledMapResource() {
+  return {
+    image: tabris.app.getResourceLocation("resources/images/floorplan.png"),
+    jQuery: tabris.app.getResourceLocation("lib/jquery-2.2.0.min.js"),
+    jQueryPanZoom: tabris.app.getResourceLocation("lib/jquery.panzoom.min.js")
+  };
+}
+
+function getRemoteMapResource() {
+  return {
+    image: "http://download.eclipsesource.com/~cpetrov/floorplan.jpg",
+    jQuery: "https://code.jquery.com/jquery-2.2.0.min.js",
+    jQueryPanZoom: "https://raw.githubusercontent.com/timmywil/jquery.panzoom/master/dist/jquery.panzoom.min.js"
+  };
+}
+
+function internetConnectionAvailable() {
+  return timeout(3000, fetch(getRemoteMapResource().image, {method: "HEAD"}));
+}
+
+function timeout(milliseconds, promise) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      reject(new Error("timeout"));
+    }, milliseconds);
+    promise.then(resolve, reject);
+  });
+}
