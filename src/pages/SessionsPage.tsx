a45f2f6c0d24d37087fb6b6283a9ad0d9657cf20
Christian Petrov <cpetrov@eclipsesource.com>
2018-03-22 09:23:00 +0100

    Migrate SessionsPage to TypeScript
    Not using data binding for TabrisConCollectionView, since it does not
    derive from Composite. TabrisConCollectionView will be refactored away
    in future changes anyway.
    
    Change-Id: I2863ea76469ebaaed7790445e1c5a16b1fa8026a

---
+++ src/pages/SessionsPage.tsx
@@ -0,0 +1,47 @@
+import TabrisConCollectionView from "../components/collectionView/TabrisConCollectionView";
+import texts from "../resources/texts";
+import { Page, ActivityIndicator } from "tabris";
+import { getById } from "tabris-decorators";
+import ViewDataProvider from "../ViewDataProvider";
+import LoginService from "../helpers/CodLoginService";
+import CodFeedbackService from "../helpers/CodFeedbackService";
+
+export default class SessionsPage extends Page {
+
+  private _data: any;
+  @getById private activityIndicator: ActivityIndicator;
+
+  constructor(viewDataProvider: ViewDataProvider, loginService: LoginService, feedbackService: CodFeedbackService) {
+    super({
+      id: "sessionsPage",
+      title: texts.SESSIONS_PAGE_TITLE_LOADING
+    });
+    this.append(
+      <widgetCollection>
+        <activityIndicator
+            id="activityIndicator"
+            centerX={0} centerY={0} />
+        <TabrisConCollectionView
+            id="sessionsList"
+            left={0} top={0} right={0} bottom={0} opacity={0}
+            viewDataProvider={viewDataProvider}
+            loginService={loginService}
+            feedbackService={feedbackService} />
+      </widgetCollection>
+    );
+  }
+
+  set data(data: any) {
+    this._data = data;
+    this.title = data.title;
+    let list = this.find("#sessionsList").first(TabrisConCollectionView);
+    list.items = data.items;
+    list.animate({opacity: 1}, {duration: 250});
+    this.activityIndicator.dispose();
+  }
+
+  get data() {
+    return this._data;
+  }
+
+}
